79021ae19d858d50ce98ecac35aeb710d6a1f7d8
Jackson <jacana_euh@hotmail.com>
2017-10-11 22:22:28 -0300

    lalala teste3

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	 System.out.println("Teste de hello world");
 	 System.out.println("Vai cata coquinho");
 	 System.out.println("Teste2");
+	 System.out.println("Teste3");
 	}
 	 
 }
