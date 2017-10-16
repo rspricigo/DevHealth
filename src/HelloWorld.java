3c9242b2cdff1c9beff8ec3b465bc8db2685d072
Jackson <jacana_euh@hotmail.com>
2017-10-16 19:10:01 -0200

    # WARNING: head commit changed in the meantime
    teste de sincronizacao

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
 	public void start() {
 	 System.out.println("Teste de hello world");
 	 System.out.println("Vai cata coquinho");
+	 System.out.println("Teste2");
+	 System.out.println("Teste3");
 	}
 	 
 }
