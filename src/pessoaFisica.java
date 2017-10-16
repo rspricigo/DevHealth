184f62197191c41f861dc21b47451c38453581fb
Jackson <jacana_euh@hotmail.com>
2017-10-16 19:33:03 -0200

    Classe Pessoa Fisica

---
+++ src/pessoaFisica.java
@@ -0,0 +1,24 @@
+
+public class pessoaFisica {
+	private Integer cdPessoaFisica;
+	private String nmPessoaFisica;
+	
+	public pessoaFisica() {
+		
+	}
+	
+	public void setCdPessoaFisica(Integer cdPessoaFisica) {
+		this.cdPessoaFisica = cdPessoaFisica;
+	}
+	public Integer getCdPessoaFisica() {
+		return this.cdPessoaFisica;
+	}
+	
+	public void setNmPessoaFisica(String nmPessoaFisica) {
+		this.nmPessoaFisica = nmPessoaFisica;
+	}
+	public String getNmPessoaFisica() {
+		return this.nmPessoaFisica;
+	}
+
+}
