fe0b01929f32621ad1fe28739235b425bf5b67c4
reginaldo <reginaldo@reginaldo-A14CU>
2017-10-17 22:10:32 -0200

    Persistencia

--- src/com/devhealth/dao/TesteDAO.java
+++ src/com/devhealth/dao/TesteDAO.java
@@ -1,5 +1,12 @@
 package com.devhealth.dao;
 
-public class TesteDAO {
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
+public class TesteDAO {
+	EntityManagerFactory factory = 
+			Persistence.createEntityManagerFactory("mydb");
+	
+	EntityManager em = factory.createEntityManager();
 }
