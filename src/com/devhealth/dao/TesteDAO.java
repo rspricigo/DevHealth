3858ec75de8c9219cb100d6ca2402c1fb715fbed
reginaldo <reginaldo@reginaldo-A14CU>
2017-10-17 23:11:21 -0200

    conexao

--- src/com/devhealth/dao/TesteDAO.java
+++ src/com/devhealth/dao/TesteDAO.java
@@ -4,9 +4,26 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import com.devhealth.entity.Procedimento;
+
 public class TesteDAO {
-	EntityManagerFactory factory = 
-			Persistence.createEntityManagerFactory("mydb");
 	
-	EntityManager em = factory.createEntityManager();
+	public static void main(String[] args) {
+		EntityManagerFactory factory = 
+				Persistence.createEntityManagerFactory("mydb");
+		
+		EntityManager em = factory.createEntityManager();
+		
+		Procedimento proc = new Procedimento();
+		
+		proc.setDs_procedimento("Teste de procedimento");
+		em.getTransaction().begin();
+		em.persist(proc);
+		em.getTransaction().commit();
+		em.close();
+		factory.close();
+		
+		
+	}
+	
 }
