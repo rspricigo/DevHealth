4806bef0b837ba8926c988b27039b756166aadff
reginaldo <reginaldo@reginaldo-A14CU>
2017-11-08 18:47:56 -0200

    commit do spring

---
+++ src/com/devhealth/logica/Logica.java
@@ -0,0 +1,14 @@
+package com.devhealth.logica;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface Logica {
+
+	
+	String executa(HttpServletRequest req,
+            HttpServletResponse res) throws Exception;
+}
+	
+	
+
---
+++ src/com/devhealth/servlet/ControllerServlet.java
@@ -0,0 +1,35 @@
+package com.devhealth.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.devhealth.logica.Logica;
+
+@WebServlet("/larissa")
+public class ControllerServlet extends HttpServlet {
+	protected void service(HttpServletRequest request,
+            HttpServletResponse response) 
+            throws ServletException, IOException {
+
+        String parametro = request.getParameter("logica");
+        String nomeDaClasse = "com.devhealth.logica." + parametro;
+
+        try {
+            Class classe = Class.forName(nomeDaClasse);
+
+            Logica logica = (Logica) classe.newInstance();
+            String pagina = logica.executa(request, response);
+
+  request.getRequestDispatcher(pagina).forward(request, response);
+
+        } catch (Exception e) {
+            throw new ServletException(
+              "A lógica de negócios causou uma exceção", e);
+        }
+    }
+}
