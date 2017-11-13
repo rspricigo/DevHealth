3dfceac9d7fe923f30955c627f4dc9aefe9555f5
reginaldo <reginaldo@reginaldo-A14CU>
2017-11-13 20:37:35 -0200

    Mapeamento Hibernate

--- src/com/devhealth/entity/ProcAgendamento.java
+++ src/com/devhealth/entity/ProcAgendamento.java
@@ -1,7 +1,13 @@
 package com.devhealth.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
 public class ProcAgendamento {
-
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int nr_sequencia;
 	private int nr_seq_agendamento;
 	private int cd_procedimento;
--- src/com/devhealth/entity/Usuario.java
+++ src/com/devhealth/entity/Usuario.java
@@ -1,7 +1,13 @@
 package com.devhealth.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
 public class Usuario {
-
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int nr_sequencia;
 	private PessoaFisica cd_pessoa_fisica;
 	private String nm_usuario;
