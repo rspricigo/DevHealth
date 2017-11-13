602737a95e9e6ad6dfd8be6e5064c7d18db04d52
reginaldo <reginaldo@reginaldo-A14CU>
2017-11-13 19:07:38 -0200

    AddPessoa

--- src/com/devhealth/controller/PessoaController.java
+++ src/com/devhealth/controller/PessoaController.java
@@ -3,10 +3,20 @@ package com.devhealth.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.devhealth.dao.PessoaFisicaDAO;
+import com.devhealth.entity.PessoaFisica;
+
 @Controller
 public class PessoaController {
 	@RequestMapping("/Pessoa")
 	public String execute() {
 		return "/Pessoa/Pessoa";
+	}
+	
+	@RequestMapping("adicionaPessoa")
+	public String adicionaPessoa(PessoaFisica pf) {
+		PessoaFisicaDAO dao = new PessoaFisicaDAO();
+		dao.save(pf);		
+		return "/Pessoa/CadastradoComSucesso";
 	}
 }
