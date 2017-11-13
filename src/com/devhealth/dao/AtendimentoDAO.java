9ee67bae29b1c9c88b079efa9593b3f6a3145002
Larissa <Larissa@DESKTOP-H7OUKFH.local>
2017-11-13 20:37:49 -0200

    DAO's

---
+++ src/com/devhealth/dao/AtendimentoDAO.java
@@ -0,0 +1,11 @@
+package com.devhealth.dao;
+
+import com.devhealth.entity.Atendimento;
+
+public class AtendimentoDAO extends GenericDAO<Atendimento, Integer> {
+
+	public AtendimentoDAO() {
+		super(Atendimento.class);
+	}
+	
+}
---
+++ src/com/devhealth/dao/ReligiaoDAO.java
@@ -0,0 +1,9 @@
+package com.devhealth.dao;
+
+import com.devhealth.entity.Religiao;
+
+public class ReligiaoDAO extends GenericDAO<Religiao, Integer> {
+	public ReligiaoDAO() {
+		super(Religiao.class);
+	}
+}
