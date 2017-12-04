42c9854e6fdc781aa98de899f5e8d6ab5121a94a
reginaldo <reginaldo@reginaldo-A14CU>
2017-12-04 00:07:58 -0200

    cadastro de pessoa - JQuery buscar mae e pai

--- src/com/devhealth/controller/PessoaController.java
+++ src/com/devhealth/controller/PessoaController.java
@@ -1,11 +1,12 @@
 package com.devhealth.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.devhealth.dao.PessoaFisicaDAO;
@@ -25,26 +26,42 @@ public class PessoaController {
 		PessoaFisicaDAO dao = new PessoaFisicaDAO();
 	//	PessoaFisica mae = dao.encontrar(id);
 	//	pf.setMae(mae);		
+		System.out.println(pf.getNm_pessoa());
 		dao.save(pf);			
 		System.out.println(pf.getCd_pessoa_fisica());
 		return lista();
 	}
 	
-	@RequestMapping("testaAjax")
-	public void testaAjax(String id) {
-		System.out.println("Cheguei aqui");
-		System.out.println(id+"Teste");
-		System.out.println(id);		
-	}
+	
 	
 	@RequestMapping("listaPessoas")
 	public ModelAndView lista() {
 		PessoaFisicaDAO dao = new PessoaFisicaDAO();
 		List<PessoaFisica> pessoas = dao.getList();
 		ModelAndView mv = new ModelAndView("Pessoa/ListaPessoa");
-		//model.addAttribute("pessoas", pessoas);
-	    mv.addObject("pessoas",pessoas);
+		mv.addObject("pessoas",pessoas);
 	    return mv;
 				
 	}
+	
+	
+	/*@RequestMapping("listaPorNome")
+	public @ResponseBody List<PessoaFisica> listaPorNome(@ModelAttribute("nm_mae") String nm_mae) {
+		PessoaFisicaDAO dao = new PessoaFisicaDAO();
+		List<PessoaFisica> pessoas = dao.getListPorNome(nm_mae);
+	    System.out.println("Lari");
+	    return pessoas;
+				
+	}*/
+	
+	@RequestMapping("listaPorNome")
+	public ModelAndView listaPorNome(String nm_pessoa) {
+		PessoaFisicaDAO dao = new PessoaFisicaDAO();
+		List<PessoaFisica> pessoas = dao.getListPorNome(nm_pessoa);
+		ModelAndView mv = new ModelAndView("Pessoa/ListaPessoa");
+		mv.addObject("pessoas",pessoas);
+	    return mv;
+	}
+	
+	
 }
--- src/com/devhealth/dao/PessoaFisicaDAO.java
+++ src/com/devhealth/dao/PessoaFisicaDAO.java
@@ -1,11 +1,44 @@
 package com.devhealth.dao;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
 import com.devhealth.entity.PessoaFisica;
+import com.devhealth.util.JPAUtil;
 
 public class PessoaFisicaDAO extends GenericDAO<PessoaFisica, Integer> {
 
 	public PessoaFisicaDAO() {
 		super(PessoaFisica.class);
 	}
+	
+	
+	public List<PessoaFisica> getListPorNome(String nome) {
+		
+		//String jpql = "Select p from PessoaFisica p where p.nm_pessoa = :nm_pessoa";
+		EntityManager em = JPAUtil.getEntityManager();
+		
+		//Query cq = em.createQuery(jpql,PessoaFisica.class);
+		//cq.setParameter("nm_pessoa", nome)
+		
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		Predicate predicate = cb.and();
+		CriteriaQuery<PessoaFisica> query = cb.createQuery(PessoaFisica.class);
+		Root<PessoaFisica> from = query.from(PessoaFisica.class);
+		TypedQuery<PessoaFisica> tp = em.createQuery(	query.select(from).where(
+				predicate = cb.and(predicate, 
+		        cb.like(from.<String>get("nm_pessoa"), "%"+nome+"%"))));
+		List<PessoaFisica> lista = tp.getResultList();
+		for(PessoaFisica p : lista) {
+			System.out.println(p.getNm_pessoa());
+		}
+		return lista;		
+	   }
 	
 }
